d2c10fcdf3a0f5416b1360da5dc5051814fa20d4
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-17 16:12:10 +0300

    Implement and export a reducer for entire cocktails list

--- src/reducers/allReducer.js
+++ src/reducers/allReducer.js
@@ -7,3 +7,27 @@ const initialState = {
   cocktails: [],
   error: '',
 };
+
+const allReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_COCKTAILS_REQUEST:
+      return {
+        ...state,
+        error: '',
+      };
+    case GET_COCKTAILS_SUCCESS:
+      return {
+        ...state,
+        cocktails: action.payload,
+      };
+    case GET_COCKTAILS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default allReducer;
