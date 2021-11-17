f522441a8bc0ab2d18590944b2cd7aeb371a7448
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-17 16:58:23 +0300

    Configure redux store

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import allReducer from './reducers/allReducer';
+
+const store = createStore(allReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
