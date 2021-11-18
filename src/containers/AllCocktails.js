4f51eb6e0273b87aa25d5423ac6aa43e525bbce6
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-18 17:08:03 +0300

    Render a button to navigate to individual page

--- src/containers/AllCocktails.js
+++ src/containers/AllCocktails.js
@@ -2,17 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import EachCocktail from './EachCocktail';
 
-const AllCocktails = ({ cocktails }) => (
-  <div>
-    {cocktails.cocktails.map((cocktail) => (
-      <>
-        <p key={cocktail.idDrink}>{cocktail.strDrink}</p>
-        <img src={cocktail.strDrinkThumb} alt={cocktail.strDrinkThumb} />
-        <EachCocktail cocktail={cocktail} />
-      </>
-    ))}
-  </div>
-);
+const AllCocktails = ({ cocktails }) => {
+  const seeCocktail = (cocktail) => {
+    <EachCocktail cocktail={cocktail} />;
+  };
+
+  return (
+    <div>
+      {cocktails.cocktails.map((cocktail) => (
+        <>
+          <p key={cocktail.idDrink}>{cocktail.strDrink}</p>
+          <img src={cocktail.strDrinkThumb} alt={cocktail.strDrinkThumb} />
+          <button type="button" onClick={() => seeCocktail(cocktail)}>See Cocktail</button>
+        </>
+      ))}
+    </div>
+  );
+};
 
 AllCocktails.propTypes = {
   cocktails: PropTypes.arrayOf(PropTypes.object).isRequired,
