6dff2f3f087195196d54479d3584909741eda923
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-19 16:47:44 +0300

    Link onto a single cocktail page by a unique id

--- src/containers/AllCocktails.js
+++ src/containers/AllCocktails.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import EachCocktail from './EachCocktail';
 
-const AllCocktails = ({ cocktails }) => {
-  const seeCocktail = (cocktail) => {
-    <Link to="/Cocktail" className="nav-link">
-      <EachCocktail cocktail={cocktail} />
-      ;
-    </Link>;
-  };
-
-  return (
-    <div>
-      {cocktails.cocktails.map((cocktail) => (
-        <>
-          <p key={cocktail.idDrink}>{cocktail.strDrink}</p>
-          <img src={cocktail.strDrinkThumb} alt={cocktail.strDrinkThumb} />
-          <button type="button" onClick={() => seeCocktail(cocktail)}>See Cocktail</button>
-        </>
-      ))}
-    </div>
-  );
-};
+const AllCocktails = ({ cocktails }) => (
+  <div>
+    {cocktails.cocktails.map((cocktail, idx) => (
+      <>
+        <p key={cocktail.idDrink}>{cocktail.strDrink}</p>
+        <img src={cocktail.strDrinkThumb} alt={cocktail.strDrinkThumb} />
+        <Link to={`/cocktail/${idx}`} className="nav-link">Cocktail</Link>
+      </>
+    ))}
+  </div>
+);
 
 AllCocktails.propTypes = {
   cocktails: PropTypes.arrayOf(PropTypes.object).isRequired,
