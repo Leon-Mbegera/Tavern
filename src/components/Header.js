b01dc73ca7ac75c6f0e1426fd0b464bf8b31a934
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-21 22:50:51 +0300

    Add a logo to my application webpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlassCheers } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
   <header>
     <nav className="navbar navbar-light">
       <div>
-        <i className="fa fa-beer" aria-hidden="true" />
         <Link to="/" className="Brand navbar-brand text-danger fs-1 fw-bolder">TAVERN</Link>
+        <span className="Logo">
+          <FontAwesomeIcon icon={faGlassCheers} />
+        </span>
       </div>
     </nav>
   </header>
