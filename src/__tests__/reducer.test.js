e5c0a75bdd5f7724e40644108f3e6c61608c831f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-23 18:25:39 +0300

    Test allCocktails reducer switch cases

---
+++ src/__tests__/reducer.test.js
@@ -0,0 +1,68 @@
+import allReducer from '../reducers/allReducer';
+import {
+  GET_COCKTAILS_REQUEST, GET_COCKTAILS_SUCCESS,
+  GET_COCKTAILS_FAILURE, GET_COCKTAIL, getCocktailsRequest, getCocktailsList, getCocktail,
+  getCocktailsSuccess, getCocktailsFailure,
+} from '../actions/index';
+
+const initialState = {
+  cocktails: [],
+  error: '',
+};
+
+describe('All Cocktails Reducer', () => {
+  test('should return the initial state', () => {
+    expect(allReducer(undefined, {})).toEqual({
+      cocktails: [],
+      error: '',
+    });
+  });
+
+  it('returned initial state should match snapshot', () => {
+    expect(allReducer(undefined, {})).toMatchSnapshot({
+      cocktails: [],
+      error: '',
+    });
+  });
+
+  test('should handle GET_COCKTAILS_REQUEST', () => {
+    expect(allReducer(initialState, GET_COCKTAILS_REQUEST)).toEqual({
+      ...initialState,
+      error: '',
+    });
+  });
+
+  test('should handle GET_COCKTAIL_SUCCESS', () => {
+    const response = {
+      cocktails: [
+        { strDrink: '155 Belmont' },
+      ],
+    };
+
+    const successAction = {
+      type: GET_COCKTAILS_SUCCESS,
+      payload: response.cocktails,
+    };
+
+    expect(allReducer(initialState, successAction)).toEqual({
+      ...initialState,
+      cocktails: successAction.payload,
+    });
+  });
+
+  test('should handle GET_COCKTAILS_FAILURE', () => {
+    const response = {
+      error: "404 Error couldn't fetch",
+    };
+
+    const failureAction = {
+      type: GET_COCKTAILS_SUCCESS,
+      payload: response.error,
+    };
+
+    expect(allReducer(initialState, failureAction)).toEqual({
+      ...initialState,
+      cocktails: failureAction.payload,
+    });
+  });
+});
