957439b6828f5e85c9502efd29861a07f70ff837
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-26 13:43:59 +0300

    Tests for the header component

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from '../components/Header';
+
+test('The header component is defined', () => {
+  const navbar = render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>,
+  );
+  expect(navbar).toBeDefined();
+});
+
+test('The header component rendered matches the snapshot', () => {
+  const navbar = renderer.create(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>,
+  ).toJSON();
+  expect(navbar).toMatchSnapshot();
+});
