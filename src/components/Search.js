1bac3bfd65940b32a666088330f09c3f1f0fca1d
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-31 16:53:06 +0300

    Use grammarly on text

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ const SearchBar = ({ handleFilterChange }) => (
   <div className="sub-header container-fluid d-flex flex-direction-column justify-content-between">
     <div className="d-flex flex-direction-column offer">
       <h4 className="fw-bold">NOVEMBER BLACK FRIDAY SALES</h4>
-      <p className="discount py-1">Up to 50% off</p>
+      <p className="discount py-1">Up to 50% Off</p>
       <p className="live-now blink">LIVE NOW</p>
     </div>
     <div className="form-outline my-3 Searchbar">
